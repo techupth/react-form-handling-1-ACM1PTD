7c99efd13283b3a6e975581db538e7eb7641ac94
Nonthawath Patchakapati <npc2540@hotmail.com>
2024-06-26 22:18:33 +0700

    adding state and event-handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [showGreetings, setShowGreetings] = useState("Greeting Meseage")
+  const [newGreetings, setNewGreetings] = useState("")
+
+  const handleNewGreetings = (event) => setNewGreetings(event.target.value);
+  const handleUpdateClick = () => setShowGreetings(newGreetings);
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{showGreetings}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input id="greeting-message" type="text" value={newGreetings} onChange={handleNewGreetings}/>
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handleUpdateClick}>Update text</button>
       </div>
     </div>
   );
